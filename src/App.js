2a06a16addb97eeca2668aba85ac9a54af1babcd
janakiabhinaya <janakiabhinayakarasala3@gamil.com>
2024-04-28 11:54:39 +0530

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Desktop } from "./components/desktopcomponent";
 import {Mobile} from "./components/mobilecomponent";
 function App(){
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [screenHeight, setScreenHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+      setScreenHeight(window.innerHeight);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
        return(
         <div>
-      <Desktop className="desktop-component" />
-      <Mobile className="mobile-component" />
+              {
+        (screenWidth < 800 || (screenHeight > screenWidth)) ? <Mobile className="mobile-component" /> : <Desktop className="desktop-component" />
+      }
       </div>
        )
 }
